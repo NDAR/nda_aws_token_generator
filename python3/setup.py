b8c11d87b6052c9c8c495a9b602d53683e9756ad
David Obenshain <david.obenshain@gmail.com>
2016-06-10 11:18:15 -0400

    Updates to support Python 2 as well as 3, updated minor version from 1.0 to 1.1

--- python3/setup.py
+++ python3/setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='nda-aws-token-generator',
-      version='1.0',
+      version='1.1',
       description='NDA AWS Token Generator',
       author='NDA',
       author_email='NDAHelp@mail.nih.gov',
