cf7bc9c1a69a614c819e652b3bfc40b66cfbe77c
David Obenshain <david.obenshain@nih.gov>
2021-04-09 06:31:09 -0400

    Update nda_aws_token_generator.py to handle hashing passwords using both SHA1 and SHA256 methods.

--- python/setup.py
+++ python/setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='nda-aws-token-generator',
-      version='1.1',
+      version='1.2',
       description='NDA AWS Token Generator',
       author='NDA',
       author_email='NDAHelp@mail.nih.gov',
